fd7795ace3486420682f6a5444706df038b788a8
Thomas, Kyle <>
2019-01-02 17:57:52 +0000

    Added some props and onclick state

--- Project/my-app/src/components/Square.js
+++ Project/my-app/src/components/Square.js
@@ -4,8 +4,8 @@ import './Square.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={() => alert('click')}>
+          {this.props.value}
         </button>
       );
     }
